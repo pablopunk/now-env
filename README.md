3434f9477381e8fcf176e2aebbd6a037f2ea409a
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-31 13:01:27 -0500

    Add secrets support

--- README.md
+++ README.md
@@ -20,3 +20,10 @@ You can now check in `process.env` for you environment variables.
 This module check if you are running in production mode and if so don't do anything. That check only work if you run your script with `NODE_ENV=production node index.js`.
 
 Because [now.sh](https://now.sh) automatically run your app with the `now.json` environment variables then this module it's not going to do anything in production :)
+
+## Using secrets
+Most probably you will want to use [secret keys](https://zeit.co/docs/features/env-and-secrets#securing-env-variables-using-secrets) in your `now.json` file. This module allow you to use them too without worries in development.
+
+Just create a `now-secrets.json` file with you development secrets, that file can (and most probably) be ignored with Git, then just use `now-env` as usual and it will auto-detect the file and use it to replace your secrets values.
+
+If the file doesn't exists or if your secret key is not defined then it's going to use the key name as value for your environment variable.
--- index.js
+++ index.js
@@ -3,27 +3,46 @@
 const { existsSync } = require('fs')
 const { resolve } = require('path')
 
-const applyEnv = env => Object.keys(env).forEach(key => {
-  process.env[key] = env[key]
-})
-
-const config = () => {
-  const NOW_PATH = resolve('./now.json')
-
-  if (existsSync(NOW_PATH)) {
-    const nowFile = require(NOW_PATH)
-
-    if (nowFile.env) {
-      return applyEnv(nowFile.env)
+const applyEnv = (env, secrets) => {
+  for (const key in env) {
+    // if the key already exists don't overwrite it
+    if (!process.env[key]) {
+      const value = secrets[env[key]] || env[key]
+      process.env[key] = value
     }
   }
+}
 
-  const PKG_PATH = resolve('./package.json')
+const loadSecrets = () => {
+  const SECRET_PATH = resolve('./now-secrets.json')
+  if (existsSync(SECRET_PATH)) {
+    return require(SECRET_PATH)
+  }
+  return {}
+}
 
-  const pkgFile = require(PKG_PATH)
+const config = () => {
+  // only run this in a non-production environment
+  if (process.env.NODE_ENV !== 'production') {
+    const secrets = loadSecrets()
 
-  if (pkgFile.now && pkgFile.now.env) {
-    return applyEnv(pkgFile.now.env)
+    const NOW_PATH = resolve('./now.json')
+
+    if (existsSync(NOW_PATH)) {
+      const nowFile = require(NOW_PATH)
+
+      if (nowFile.env) {
+        return applyEnv(nowFile.env, secrets)
+      }
+    }
+
+    const PKG_PATH = resolve('./package.json')
+
+    const pkgFile = require(PKG_PATH)
+
+    if (pkgFile.now && pkgFile.now.env) {
+      return applyEnv(pkgFile.now.env, secrets)
+    }
   }
 }
 
